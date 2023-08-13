33c1c0df2d9af973227c359f6644ae062129f49d
IpsaJoshi <ipsa7777@gmail.com>
2023-08-13 12:45:16 -0400

    Color code removed

--- app.py
+++ app.py
@@ -11,7 +11,6 @@ DBHOST = os.environ.get("DBHOST") or "localhost"
 DBUSER = os.environ.get("DBUSER") or "root"
 DBPWD = os.environ.get("DBPWD") or "passwors"
 DATABASE = os.environ.get("DATABASE") or "employees"
-COLOR_FROM_ENV = os.environ.get('APP_COLOR') or "lime"
 DBPORT = int(os.environ.get("DBPORT"))
 background_image_url = os.environ.get("background_image_url") or "https://bg-images-grp14.s3.amazonaws.com/img1.jpeg"
 
@@ -27,24 +26,6 @@ db_conn = connections.Connection(
 output = {}
 table = 'employee';
 
-# Define the supported color codes
-color_codes = {
-    "red": "#e74c3c",
-    "green": "#16a085",
-    "blue": "#89CFF0",
-    "blue2": "#30336b",
-    "pink": "#f4c2c2",
-    "darkblue": "#130f40",
-    "lime": "#C1FF9C",
-}
-
-
-# Create a string of supported colors
-SUPPORTED_COLORS = ",".join(color_codes.keys())
-
-# Generate a random color
-COLOR = random.choice(["red", "green", "blue", "blue2", "darkblue", "pink", "lime"])
-
 def download_file(file_name, bucket):
     """
     Function to download a given file from an S3 bucket
@@ -57,11 +38,11 @@ def download_file(file_name, bucket):
 
 @app.route("/", methods=['GET', 'POST'])
 def home():
-    return render_template('addemp.html', color=color_codes[COLOR], background_image_url= "https://bg-images-grp14.s3.amazonaws.com/img1.jpeg")
+    return render_template('addemp.html', background_image_url= "https://bg-images-grp14.s3.amazonaws.com/img1.jpeg")
 
 @app.route("/about", methods=['GET','POST'])
 def about():
-    return render_template('about.html', color=color_codes[COLOR], background_image_url= "https://bg-images-grp14.s3.amazonaws.com/img1.jpeg")
+    return render_template('about.html', background_image_url= "https://bg-images-grp14.s3.amazonaws.com/img1.jpeg")
 
 @app.route("/download/<filename>", methods=['GET'])
 def download(filename):
@@ -93,11 +74,11 @@ def AddEmp():
         cursor.close()
 
     print("all modification done...")
-    return render_template('addempoutput.html', name=emp_name, color=color_codes[COLOR], background_image_url= "https://bg-images-grp14.s3.amazonaws.com/img1.jpeg")
+    return render_template('addempoutput.html', name=emp_name, background_image_url= "https://bg-images-grp14.s3.amazonaws.com/img1.jpeg")
 
 @app.route("/getemp", methods=['GET', 'POST'])
 def GetEmp():
-    return render_template("getemp.html", color=color_codes[COLOR], background_image_url= "https://bg-images-grp14.s3.amazonaws.com/img1.jpeg")
+    return render_template("getemp.html", background_image_url= "https://bg-images-grp14.s3.amazonaws.com/img1.jpeg")
 
 
 @app.route("/fetchdata", methods=['GET','POST'])
@@ -126,29 +107,7 @@ def FetchData():
         cursor.close()
 
     return render_template("getempoutput.html", id=output["emp_id"], fname=output["first_name"],
-                           lname=output["last_name"], interest=output["primary_skills"], location=output["location"], color=color_codes[COLOR], background_image_url= "https://bg-images-grp14.s3.amazonaws.com/img1.jpeg")
+                           lname=output["last_name"], interest=output["primary_skills"], location=output["location"], background_image_url= "https://bg-images-grp14.s3.amazonaws.com/img1.jpeg")
 
-if __name__ == '__main__':
-    
-    # Check for Command Line Parameters for color
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--color', required=False)
-    args = parser.parse_args()
-
-    if args.color:
-        print("Color from command line argument =" + args.color)
-        COLOR = args.color
-        if COLOR_FROM_ENV:
-            print("A color was set through environment variable -" + COLOR_FROM_ENV + ". However, color from command line argument takes precendence.")
-    elif COLOR_FROM_ENV:
-        print("No Command line argument. Color from environment variable =" + COLOR_FROM_ENV)
-        COLOR = COLOR_FROM_ENV
-    else:
-        print("No command line argument or environment variable. Picking a Random Color =" + COLOR)
-
-    # Check if input color is a supported one
-    if COLOR not in color_codes:
-        print("Color not supported. Received '" + COLOR + "' expected one of " + SUPPORTED_COLORS)
-        exit(1)
-
+if __name__ == '__main__':    
     app.run(host='0.0.0.0',port=8080,debug=True)
